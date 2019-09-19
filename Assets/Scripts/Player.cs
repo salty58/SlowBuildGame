3fcfb8bb98826ab2eddd15ded8532ac129bf49d0
salty68 <lck295@nyu.edu>
2019-09-19 17:12:32 -0400

    Second Commit
    Added movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float horizontal;
+
+    float slothRun = 0.5f;
+
+    Rigidbody2D myRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        myRigidBody.velocity = new Vector2(horizontal * slothRun, 0f);
+    }
+}
